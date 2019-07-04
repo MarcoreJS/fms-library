5084e306ffef9f63f62c7aec89de71c3398d67ce
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2019-07-04 02:07:55 -0500

    v1.0

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,77 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12>
-      <v-card class="pa-4">
+  <v-container grid-list-xl fluid>
+    <v-layout row wrap>
+      <v-flex md6>
+        <v-text-field v-model="book.title" label="Titulo"></v-text-field>
+      </v-flex>
+      <v-flex md6>
+        <v-text-field v-model="book.author" label="Author"></v-text-field>
+      </v-flex>
+      <v-flex md4>
+        <v-text-field v-model="book.publisher" label="Editorial"></v-text-field>
+      </v-flex>
+      <v-flex md4>
+        <v-text-field v-model="book.category" label="Categoria"></v-text-field>
+      </v-flex>
+      <v-flex md2>
+        <v-text-field v-model="book.price" label="Precio"></v-text-field>
+      </v-flex>
+      <v-flex md2>
+        <v-text-field v-model="book.onStock" label="Inventario" mask="####"></v-text-field>
+      </v-flex>
+      <v-flex xs12>
         <label>
-          <v-icon>attach_file</v-icon>Book Image:
+          <v-icon>attach_file</v-icon>Foto del Libro:
         </label>
         <input type="file" :multiple="false" ref="fileInput" @change="onFileChange" />
-      </v-card>
-    </v-flex>
+      </v-flex>
 
-    <v-flex xs12 text-xs-right>
-      <v-btn class="mx-0 font-weight-light" color="success" @click="newBook()">Guardar</v-btn>
-    </v-flex>
-  </v-layout>
+      <v-flex xs12 text-xs-right>
+        <v-btn class="mx-0 font-weight-light" color="success" @click="newBook()">Guardar</v-btn>
+      </v-flex>
+
+      <v-flex sm12>
+        <v-data-table
+          class="elevation-1"
+          :rows-per-page-items="[10, 25, 100, 200]"
+          :headers="headers"
+          :items="books"
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.title }}</td>
+            <td>{{ props.item.author }}</td>
+            <td>{{ props.item.category }}</td>
+            <td>{{ props.item.price }}</td>
+            <td>{{ props.item.onStock }}</td>
+            <td>
+              <v-btn
+                :to="{ name: 'libro-sk', params: { sk: props.item.sk, book: props.item }}"
+                outline
+              >Ver</v-btn>
+            </td>
+            <td>
+              <v-btn @click="deleteBook(props.item.sk)" color="red" outline>Eliminar</v-btn>
+            </td>
+          </template>
+        </v-data-table>
+
+        <v-dialog v-model="dialog" width="300">
+          <v-card>
+            <v-card-text>
+              <h3 v-text="text"></h3>
+              <v-progress-linear
+                :active="show"
+                :indeterminate="query"
+                color="#BF9B30"
+                class="mb-0"
+                max="100"
+              ></v-progress-linear>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -26,29 +85,52 @@ export default {
   },
   data() {
     return {
+      show: true,
+      text: "Cargando...",
+      dialog: false,
+      query: true,
+      headers: [
+        { text: "Titulo", value: "title" },
+        { text: "Autor", value: "author" },
+        { text: "Categoria", value: "category" },
+        { text: "Precio", value: "price" },
+        { text: "En Inventario", value: "onStock" },
+        { text: "Ver" },
+        { text: "Eliminar" }
+      ],
       config: {
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          "x-api-key": "J2ZdrUISFb8p1677T8EoZ8RAaoAX4nJG6q31OJp3",
           "Content-Type": "application/json"
         }
-			},
+      },
       url: "https://dniv5uxo5i.execute-api.us-west-1.amazonaws.com/dev/books",
       book: {
-        title: "Prueba",
-        author: "Prueba",
-        publisher: "Prueba",
-        category: "Prueba",
-        onStock: 12,
-        price: 12,
+        title: "",
+        author: "",
+        publisher: "",
+        category: "",
+        onStock: 0,
+        price: 0,
         image: ""
-      }
+      },
+      books: []
     };
   },
   methods: {
     newBook() {
-      this.$axios.post(this.url, this.book, this.config.headers).then(res => {
-        console.log(res)
-      }).catch(e => console.log(e.message));
+      this.dialog = true;
+      this.$axios
+        .post(this.url, this.book, this.config)
+        .then(res => {
+          console.log(res);
+          let aux_res = JSON.parse(res.data.body);
+          alert("Registrado: " + aux_res.Item.title);
+          this.getBooks();
+          this.dialog = false;
+          this.query = false;
+        })
+        .catch(e => console.log(e.message));
     },
     onFileChange($event) {
       const files = $event.target.files || $event.dataTransfer.files;
@@ -66,7 +148,31 @@ export default {
     getFormData(files) {
       const data = new FormData();
       return data;
+    },
+    getBooks() {
+      this.$axios
+        .get(
+          "https://qo62e2m977.execute-api.us-west-1.amazonaws.com/dev",
+          this.config
+        )
+        .then(res => {
+          this.books = res.data.body.Items;
+        });
+    },
+    deleteBook(sk) {
+      this.$axios
+        .delete(
+          "https://qo62e2m977.execute-api.us-west-1.amazonaws.com/dev/books/" + sk,
+          this.config
+        )
+        .then(res => {
+          this.getBooks();
+          alert("Eliminado")
+        }).catch(e => alert(e.message))
     }
+  },
+  created() {
+    this.getBooks();
   }
 };
 </script>
---
+++ pages/libro/_sk.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout row wrap>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img :src="book.image" aspect-ratio="2.75"></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ book.title }}</h3>
+              <div>{{ book.author }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn to="/" flat color="orange">Regresar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      book: this.$route.params.book,
+      sk: this.$route.params.sk
+    };
+  }
+};
+</script>
+
